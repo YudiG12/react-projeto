8966b539e370d6878b4ea96ff2e413f04268d09a
Aluno <Aluno@Digital.net>
2019-05-07 19:52:05 -0300

    logo no login

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,14 +1,23 @@
 import React, {Component} from 'react'
-import {Card} from '@material-ui/core'
+import {Card, Grid, CardMedia} from '@material-ui/core'
 import './styles.css'
+import logo from './txtlogo-deitado.png'
 
 class Login extends Component{
 
     render() {
         return (
-                <Card>
-                    oi
+            <div style={{height: '100vh'}}>
+            <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center">
+                <Card className='card'>
+                    <img alt='' src={logo}/>
                 </Card>
+            </Grid>
+            </div>
         )
     }
 
