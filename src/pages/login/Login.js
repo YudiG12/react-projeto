577c7816864e845902f4285bf85769c142b281d7
Aluno <Aluno@Digital.net>
2019-05-08 18:37:30 -0300

    tentando styles props

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,27 +1,42 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types';
 import {Card, Grid, TextField} from '@material-ui/core'
+import { withStyles} from '@material-ui/core/styles'
+import purple from '@material-ui/core/colors/purple'
 import './styles.css'
 import logo from './txtlogo-deitado.png'
 
-class Login extends Component{
 
-    render() {
-        return (
-            <Grid container direction="row" justify="center" alignItems="center">
-                <Card className='card'>
-                    <img alt='' src={logo}/><br/>
-                    <Grid container direction="row" justify="center" alignItems="center">
-                        <TextField style={{cssLabel}} label="CPF ou CNPJ" margin="normal"/>
-                    </Grid>
-                </Card>
-            </Grid>
-        )
-    }
-    // cssLabel: {
-    //     '&$cssFocused': {
-    //       color: purple[500],
-    //     },
-    //   },
+const styles = theme => ({
+    cssLabel: {
+        '&$cssFocused': {
+          color: purple[500],
+        },
+      }
+})
+
+function Login(props) {
+    const {classes} = props
+
+    return (
+        <Grid container direction="row" justify="center" alignItems="center">
+            <Card className='card'>
+                <img alt='' src={logo}/><br/>
+                <Grid container direction="row" justify="center" alignItems="center">
+                    <TextField InputLabelProps={{
+                        classes: {
+                            root: classes.cssLabel
+                        }
+                        }}
+                        label="CPF ou CNPJ" margin="normal"/>
+                </Grid>
+            </Card>
+        </Grid>
+    )
 }
 
-export default Login
+Login.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Login);
