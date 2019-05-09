811381919e83310bde401cc135c3c7a63c2b86a5
Aluno <Aluno@Digital.net>
2019-05-09 18:13:40 -0300

    login finalizada

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -7,14 +7,18 @@ function Login(props) {
 
     return (
         <Grid style={{height: '100vh'}} container direction="row" justify="center" alignItems="center">
-            <Card className='card' style={{maxWidth:'30%'}}>
-                <Grid container direction="row" justify="center" alignItems="center">
+            <Card className='card' style={{padding:'30px', paddingBottom:'0', maxWidth:'400px'}}>
+                <Grid  container direction="row" justify="center" alignItems="center">
                     <img alt='' style={{width:'60vh', marginTop:'3%'}}  src={logo}/><br/>
-                    <TextField style={{marginLeft: '11%', marginRight: '11%', marginTop: '0'}} fullWidth label="CPF ou CNPJ" margin="normal"/>
+                    <TextField style={{marginLeft: '11%', marginRight: '11%', marginTop: '-20px'}} fullWidth label="CPF ou CNPJ" margin="normal"/>
                     <TextField style={{marginLeft: '11%', marginRight: '11%'}} fullWidth label="Senha" type="password" margin="normal"/>
-                    <Button style={{margin:'11%'}} fullWidth variant="contained" color="secondary">
-                        Secondary
+                    <Grid container direction='row' justify='flex-end'>
+                        <a href='#' style={{textDecoration:'none', marginRight:'11%'}}>Esqueci minha senha</a>
+                    </Grid>
+                    <Button style={{margin:'11%',marginTop:'10%', marginBottom:'3%', height:'50px', borderRadius:'0', boxShadow:'none', backgroundColor:'#ff3f3f'}} fullWidth variant="contained" color="secondary">
+                        Login
                     </Button>
+                    <p style={{marginBottom:'8%'}}><span style={{color:'rgb(96,103,112)'}}>Não tem uma conta?</span> <a>Cadastre-se!</a></p>
                 </Grid>
             </Card>
         </Grid>
