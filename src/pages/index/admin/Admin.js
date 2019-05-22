d3589a51915a5c870bca62f88b77efea1e73d120
Aluno Bandtec <github@alunos.bandtec.com.br>
2019-05-22 17:09:32 -0300

    mock dados

--- src/pages/index/admin/Admin.js
+++ src/pages/index/admin/Admin.js
@@ -21,21 +21,39 @@ const styles = theme => ({
 });
 
 class Admin extends Component {
+  state = {
+    value: Math.random() * 100,
+    valueColor: 'rgba(255,255,255,0.8)'
+  };
+  
+  componentDidMount() {
+    this.interval = setInterval(()=> {
+      let randomVar = Math.random()*100
+      this.setState({value: randomVar}) },500)
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
   
   renderTeam1 = () => {
+    let { value, valueColor } = this.state
     let final = []
     let playerDatas = []
+    let value1, value2
     for (let i = 0; i < 5; i++) {
-      playerDatas.push(new MachineMetricData(33,33,78,87,33,33,33,
+      value1 = (value*Math.random()).toFixed(2)
+      value2 = (value*Math.random()).toFixed(2)
+
+      playerDatas.push(new MachineMetricData(33,33,value1,value2,33,33,33,
         'usbDevice',
         'metricDate',
         'metricTime',1,1))
     }
     for (let i = 0; i < playerDatas.length; i++) {
       final.push(<Typography inline style={{color:'#96a0a0', fontSize: '1.06rem', fontFamily: 'inherit'}}>GPU&nbsp;</Typography>)
-      final.push(<Typography inline style={{color:'rgba(255,255,255,0.8)', fontSize: '1.2rem', fontFamily: 'inherit'}}>{playerDatas[i].useCPU+'%'}<br/></Typography>)
+      final.push(<Typography inline style={{color:valueColor, fontSize: '1.2rem', fontFamily: 'inherit'}}>{playerDatas[i].useCPU+'%'}<br/></Typography>)
       final.push(<Typography inline style={{color:'#96a0a0', fontSize: '1.06rem', fontFamily: 'inherit'}}>CPU&nbsp;</Typography>)
-      final.push(<Typography inline style={{color:'rgba(255,255,255,0.8)', fontSize: '1.2rem', fontFamily: 'inherit'}}>{playerDatas[i].useGPU+'%'}</Typography>) 
+      final.push(<Typography inline style={{color:valueColor, fontSize: '1.2rem', fontFamily: 'inherit'}}>{playerDatas[i].useGPU+'%'}</Typography>) 
       final.push(<br />)
       if (!(i === playerDatas.length - 1))
         final.push(<Divider style={{margin: '16px', marginLeft:'15%', marginRight:'15%', backgroundColor:'rgba(255,255,255,0.15)', widht:'60%'}}/>)
@@ -44,10 +62,11 @@ class Admin extends Component {
   }
 
   renderTeam2 = () => {
+    let { value } = this.state
     let final = []
     let playerDatas = []
     for (let i = 0; i < 5; i++) {
-      playerDatas.push(new MachineMetricData(33,33,67,55,33,33,33,
+      playerDatas.push(new MachineMetricData(33,33,(value*Math.random()).toFixed(2),(value*Math.random()).toFixed(2),33,33,33,
         'usbDevice',
         'metricDate',
         'metricTime',1,1))
@@ -68,6 +87,7 @@ class Admin extends Component {
 
   render() {
     const { classes } = this.props
+
     return(
         <div className={classes.root}>
           <Grid container spacing={12}>
