adf654858c0830bcd94778ecba82ad2fc716a419
gustavoHcano <gustavoherreroc@gmail.com>
2019-05-31 10:27:25 -0300

    convites atualizado

--- src/pages/index/convites/Convites.js
+++ src/pages/index/convites/Convites.js
@@ -4,12 +4,7 @@ import { withStyles } from '@material-ui/core/styles'
 import { Grid, Card, Button, FormControl, InputLabel, MenuItem,Select, Input, CardContent } from '@material-ui/core'
 import { red } from '@material-ui/core/colors';
 import CardActions from '@material-ui/core/CardActions';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+
 
 const styles = theme => ({
     root: {
@@ -18,11 +13,7 @@ const styles = theme => ({
             paddingLeft: '250px',
         },
     },
-    table:{
-        backgroundColor:'#383c42',
-        color: '#96a0a0',
-        fontSize: '15px'
-    },
+    
     card: {
         backgroundColor: '#383c42',
         padding: theme.spacing.unit * 2,
@@ -72,33 +63,41 @@ const styles = theme => ({
 
 });
 
-let data = [1,2];
-let rows = [];
-let id = 0;
-function createData(nomeJogador, campeonato, status) {
-    id += 1;
-    return { nomeJogador, campeonato, status };
-}
-for(let i=0;data.length>i;i++){
-  rows.push( createData('nomeJogador', 'campeonato', 'status'));
-};
+
 class Convites extends Component {
     state = {
-        campeonato: '',
-        open: false,
+        userData: '',
+        
     };
-
-    handleChange = event => {
-        this.setState({ [event.target.name]: event.target.value });
-    };
-
-    handleClose = () => {
-        this.setState({ open: false });
-    };
-
-    handleOpen = () => {
-        this.setState({ open: true });
-    };
+    handleUserData(text) {
+        this.setState({ userData: text.target.value })
+    }
+    validateUserData = (data) => {
+        let dataPerson = data.replace(/\D+/g, '');
+        this.setState({userData:dataPerson});
+        if(dataPerson.length =!11){
+            alert('CPF inválido')
+        }else{
+            var cpf = dataPerson;
+            var digitoDigitado = Number(cpf.charAt(9) + cpf.charAt(10));
+            var soma1 = 0, soma2 = 0;
+            var vlr = 11;
+      
+            for (let i = 0; i < 9; i++) {
+              soma1 += Number(cpf.charAt(i) * (vlr - 1));
+              soma2 += Number(cpf.charAt(i) * vlr);
+              vlr--;
+            }
+            soma1 = (((soma1 * 10) % 11) == 10 ? 0 : ((soma1 * 10) % 11));
+            soma2 = (((soma2 + (2 * soma1)) * 10) % 11);
+      
+            var digitoGerado = (soma1 * 10) + soma2;
+            if (digitoGerado != digitoDigitado) {
+              alert('CPF Invalido!');
+              this.setState({ userData: "" });
+            }
+          }
+        }
 
     render() {
         const { classes } = this.props
@@ -109,35 +108,11 @@ class Convites extends Component {
                         <Card className={classes.card}>
                             <CardContent>
                                 <p style={{ color: '#ff3f3f', fontSize: '20px', marginTop: '-10' }}>Novo Convite</p>
-                                <FormControl fullWidth className={classes.margin}>
+                                <FormControl style={{ marginLeft: '11%', marginRight: '11%', marginTop: 'px' }} fullWidth className={classes.margin}>
                                     <InputLabel classes={{ root: classes.cssLabel, focused: classes.cssFocused }}>
-                                        Nome do Jogador
-                            </InputLabel>
-                                    <Input inputProps={{ className: classes.input }} id="playerName" classes={{ underline: classes.cssUnderline }} />
-                                </FormControl>
-                                <FormControl fullWidth className={classes.margin}>
-                                    <InputLabel classes={{ root: classes.cssLabel, focused: classes.cssFocused }}>
-                                        Email do Jogador
-                            </InputLabel>
-                                    <Input inputProps={{ className: classes.input }} id="playerEmail" classes={{ underline: classes.cssUnderline }} />
-                                </FormControl>
-                                <FormControl fullWidth className={classes.margin}>
-                                    <InputLabel classes={{ root: classes.cssLabel, focused: classes.cssFocused }}>Campeonatos</InputLabel>
-                                    <Select
-                                        className={classes.cssUnderline}                                        
-                                        inputProps={{
-                                            name: 'campeonato',
-                                            id: 'campeonato',
-                                            className: classes.input
-                                        }}
-                                    >
-                                        <MenuItem classes={{ root: classes.cssLabel}} value="">
-                                            <em>None</em>
-                                        </MenuItem>
-                                        <MenuItem value={"lol"}>LOL</MenuItem>
-                                        <MenuItem value={"cs"}>CS GO</MenuItem>
-                                        <MenuItem value={"fortnite"}>Fortnite</MenuItem>
-                                    </Select>
+                                      CPF do Jogador 
+                                    </InputLabel>
+                                    <Input inputProps={{ className: classes.input }} id="userData" classes={{ underline: classes.cssUnderline }} type="text" value={this.state.userData} onBlur={(text) => { this.validateUserData(this.state.userData) }} onChange={(text) => { this.handleUserData(text) }} />
                                 </FormControl>
                                 <CardActions>
                                     <Button size="small" id='button' type='submit' style={{ fontWeight: '300', a: 'none', margin: '11%', marginTop: '10%', marginBottom: '3%', height: '50px', borderRadius: '0', boxShadow: 'none', backgroundColor: '#ff3f3f' }} fullWidth variant="contained" color="secondary">
@@ -146,34 +121,7 @@ class Convites extends Component {
                                 </CardActions>
                             </CardContent>
                         </Card>
-                    </Grid>
-                    <Grid item xs={12} lg={6}>
-                        <Card className={classes.card}>
-                            <CardContent>
-                            <p style={{background:'#383c42', color: '#ff3f3f', fontSize: '20px', marginTop: '-10' }}>Convite Enviados</p>
-                                <Paper >
-                                    <Table className={classes.table}>
-                                        <TableHead className={classes.table} >
-                                            <TableRow >
-                                                <TableCell className={classes.table}>Jogador</TableCell>
-                                                <TableCell className={classes.table} align="right">Campeonato</TableCell>
-                                                <TableCell className={classes.table} align="right">Status</TableCell>
-                                            </TableRow>
-                                        </TableHead>
-                                        <TableBody>
-                                            {rows.map(row => (
-                                                <TableRow key={row.id} >                                                    
-                                                    <TableCell align="right" className={classes.table}>{row.nomeJogador}</TableCell>
-                                                    <TableCell align="right" className={classes.table}>{row.campeonato}</TableCell>
-                                                    <TableCell align="center" className={classes.table}>{row.status}</TableCell>
-                                                </TableRow>
-                                            ))}
-                                        </TableBody>
-                                    </Table>
-                                </Paper>
-                            </CardContent>
-                        </Card>
-                    </Grid>
+                    </Grid>                    
                 </Grid>
             </div>
         )
