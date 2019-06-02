0b886b15239cfd560568228bb1c82da322294fa6
Ariel Magalhães Lira <ariel.lira@c6bank.com>
2019-06-02 00:37:18 -0300

    invite screen with database

--- src/pages/index/player/invite/Invite.js
+++ src/pages/index/player/invite/Invite.js
@@ -5,6 +5,8 @@ import { withStyles } from '@material-ui/core/styles'
 import { Grid, Card, Typography, Button } from '@material-ui/core'
 import { red } from '@material-ui/core/colors'
 import './Invite.css'
+import data from '../../../../scripts/http/data'
+import player from '../../../../scripts/http/player'
 
 const styles = theme => ({
     root: {
@@ -54,32 +56,126 @@ const styles = theme => ({
 
 class Admin extends Component {
 
-    render(invite) {
-        const { classes } = this.props
-        return(
-            <Grid style={{height: '100vh'}} container direction="row" justify="center" alignItems="center">
-                <Card className={classes.cardResponsive} style={{ paddingBottom:'0', maxWidth:'400px'}}>
-                    <Grid  container direction="row" justify="center" alignItems="center" style={{padding: '3%'}}>
+  constructor(props) {
+    super(props);
 
-                        <img style={{ width: '80px', height: '75px'}} alt='' />
+    this.state = {
+      makeRequest: false,
+      invites: []
+    }
 
-                        <Typography align='center' variant='subtitle1' style={{color:'#96a0a0', padding: '3%'}} className={`cardPart1`}>Fulano, você foi convidado para particpar do campeonato NOME_CAMPEONATO</Typography>
+    data
+      .isLogOn(() => {
+        data.isJogador((idPlayerRole) => {
+          return this.getAllInvitesAvailable();
+        }, () => {
+          window.location.href = "/"
+        })
+      }, () => {
+        window.location.href = "/login"
+      })
+      
+  }
 
-                        <Typography align='center' variant='subtitle1' style={{color:'#96a0a0', padding: '3%'}} >Organizado por: ORGANIZADOR</Typography>
+  getAllInvitesAvailable = () => {
+    player.invitesNotAnswered()
+    .then(invites => {
+      this.setState({ invites: [] })
+      let invitesRender = []
+      if(invites.length == 0){
+        invitesRender.push(this.renderWithoutInvite())
+      }else{
+        for(let i = 0; i < invites.length; i++) {
+          invitesRender.push(this.renderInvite(invites[i]));
+        }
+      }
+      this.setState({ invites: invitesRender })
+      this.setState({ makeRequest: true })
+    })
+    .catch(err => console.log(err));
+  }
 
-                        <Typography align='center' variant='subtitle1' style={{color:'#96a0a0', padding: '3%'}} >Data: DATA_DO_CAMPEONATO</Typography>
+  renderInvites = () => {
+    return (
+      <div>
+        { this.state.invites }
+      </div>
+    )
+  }
 
-                        <Button id='button' type='submit' style={{fontWeight: '300', a: 'none', height:'50px', borderRadius:'0', boxShadow:'none', backgroundColor:'#ff3f3f', margin: '3%'}} variant="contained" color="secondary">
-                            Não aceitar
-                        </Button>
-                        
-                        <Button id='button' type='submit' style={{fontWeight: '300', a: 'none', height:'50px', borderRadius:'0', boxShadow:'none', backgroundColor:'#00d52a', margin: '3%'}} variant="contained" color="secondary">
-                            Aceitar
-                        </Button>
-                    </Grid>
-                </Card>
+  refuseInvite = (idChampionship) => {
+      this.setState({ makeRequest: false })
+      player.refuseInvite(idChampionship)
+        .then(resultado => {
+          return this.getAllInvitesAvailable();
+        })
+        .catch(err => console.log(err));
+  }
+
+  acceptInvite = (idChampionship) => {
+    this.setState({ makeRequest: false })
+    player.acceptInvite(idChampionship)
+      .then(resultado => {
+        return this.getAllInvitesAvailable();
+      })
+      .catch(err => console.log(err));
+  }
+
+  renderInvite = (invite) => {
+    const { classes } = this.props
+    return (
+      
+      <Grid style={{height: '100vh'}} container direction="row" justify="center" alignItems="center">
+        <Card className={classes.cardResponsive} style={{ paddingBottom:'0', maxWidth:'400px'}}>
+            <Grid  container direction="row" justify="center" alignItems="center" style={{padding: '3%'}}>
+
+                <img style={{ width: '80px', height: '75px'}} alt='' src="https://raw.githubusercontent.com/YudiG12/CarbonTowerEtc/master/Design/Logos-PNG/logo2-red.png"/>
+
+                <Typography align='center' variant='subtitle1' style={{color:'#96a0a0', padding: '3%'}} className={`cardPart1`}>Você foi convidado para particpar do campeonato {invite.nmChampionship}</Typography>
+
+                <Typography align='center' variant='subtitle1' style={{color:'#96a0a0', padding: '3%'}} >Que será no jogo: {invite.nmGame}</Typography>
+
+                <Button id='button' type='submit' onClick={() => {this.refuseInvite(invite.idChampionship)}} style={{fontWeight: '300', a: 'none', height:'50px', borderRadius:'0', boxShadow:'none', backgroundColor:'#ff3f3f', margin: '3%'}} variant="contained" color="secondary">
+                    Não aceitar
+                </Button>
+                
+                <Button id='button' type='submit' onClick={() => {this.acceptInvite(invite.idChampionship)}} style={{fontWeight: '300', a: 'none', height:'50px', borderRadius:'0', boxShadow:'none', backgroundColor:'#00d52a', margin: '3%'}} variant="contained" color="secondary">
+                    Aceitar
+                </Button>
+            </Grid>
+        </Card>
+      </Grid>
+
+    )
+  }
+
+  renderWithoutInvite = () => {
+    const { classes } = this.props
+    return (
+      
+      <Grid style={{height: '100vh'}} container direction="row" justify="center" alignItems="center">
+        <Card className={classes.cardResponsive} style={{ paddingBottom:'0', maxWidth:'400px'}}>
+            <Grid  container direction="row" justify="center" alignItems="center" style={{padding: '3%'}}>
+
+                <img style={{ width: '80px', height: '75px'}} alt='' src="https://raw.githubusercontent.com/YudiG12/CarbonTowerEtc/master/Design/Logos-PNG/logo2-red.png"/>
+
+                <Typography align='center' variant='subtitle1' style={{color:'#96a0a0', padding: '3%'}} className={`cardPart1`}>Você não foi convidado para particpar de nenhum campeonato no momento</Typography>
 
             </Grid>
+        </Card>
+      </Grid>
+
+    )
+  }
+
+    render() {
+        
+        return(
+          this.state.makeRequest == true ? (
+            this.renderInvites()
+          ) : (
+            <img src="https://upload.wikimedia.org/wikipedia/commons/3/3a/Gray_circles_rotate.gif"/> 
+          )
         )
     }
 }
